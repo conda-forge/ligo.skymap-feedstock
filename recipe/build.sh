14c9226d37b1e6374e980f2d4c9de5742fc5442a
Leo Singer <leo.singer@ligo.org>
2020-08-18 11:53:55 -0400

    No LTO

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ export CC="$BUILD_PREFIX/bin/clang"
 export CXX="$BUILD_PREFIX/bin/clang++"
 export LDSHARED="$BUILD_PREFIX/bin/clang -pthread -shared"
 
-export CFLAGS="${CFLAGS} -O3 -flto -fopenmp=libiomp5"
+export CFLAGS="${CFLAGS} -O3 -fopenmp=libiomp5"
 export CPPFLAGS="${CPPFLAGS} -O3 -fopenmp=libiomp5"
 export LIGO_SKYMAP_USE_SYSTEM_CHEALPIX=1
 
