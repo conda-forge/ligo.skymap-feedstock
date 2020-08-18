cbf03922b4f96f42283f187a41fc2535069c035e
Leo Singer <leo.singer@ligo.org>
2020-08-18 10:16:16 -0400

    WIP

---
+++ recipe/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -x
+
+export CC="$BUILD_PREFIX/bin/clang"
+export CXX="$BUILD_PREFIX/bin/clang++"
+
+export CFLAGS="${CFLAGS} -O3 -flto -fopenmp=libiomp5"
+export CPPFLAGS="${CPPFLAGS} -O3 -fopenmp=libiomp5"
+export LIGO_SKYMAP_USE_SYSTEM_CHEALPIX=1
+
+{{ PYTHON }} setup.py install --old-and-unmanageable
